62b85d590a5a4c7cc97d78cbe800ded3e9edc891
henunix1672 <henunix@gmail.com>
2025-06-29 19:43:33 +0700

    Sesi 13

---
+++ test/appdemo.test.js
@@ -0,0 +1,11 @@
+import {expect} from 'chai';
+
+//nama Test Suite
+describe('Calculator Demo Testing', () => {
+
+    //nama Test Case
+    it('coba test buka aplikasi calculator', async () => {
+        console.log('Calculator terbuka');
+    });
+
+})
